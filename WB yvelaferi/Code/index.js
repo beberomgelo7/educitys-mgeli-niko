09db64d9ebffcca20a105c5c25b6dcb3dbff3a0c
beberomgelo7 <beberomgelo7@gmail.com>
2026-02-15 12:58:42 +0400

    Add files via upload

---
+++ WB yvelaferi/Code/index.js
@@ -0,0 +1,13 @@
+let number = 20
+let divisor = 10
+if (divisor = 0) {
+    console.log("ნულზე გაყოფა არ შეიძლება");
+}
+else if (number % divisor === 0) {
+    console.log(number + "გაიყო" + divisor)
+} else {
+    console.log(number + "არ იყოფა" + divisor)
+}
+
+ 
+
